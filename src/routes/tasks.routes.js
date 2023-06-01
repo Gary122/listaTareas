a3a76fdb4304bb818f8d55191298f556f66a8649
Gary122 <jc_alt64@hotmail.com>
2023-06-01 10:14:45 -0500

    Rutas estables

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -2,8 +2,26 @@ const { Router } =require('express');
 
 const router = Router();
 
-router.get('/',(req,res)=>{
-    res.send('hola mundo')
+router.get('/tasks',(req,res)=>{
+    res.send('retornar una lista de tareas');
 })
+
+router.get('/tasks/10',(req,res)=>{
+    res.send('crear una sola lista de tareas');
+})
+
+router.post('/tasks',(req,res)=>{
+    res.send('crear una lista de tareas');
+})
+
+router.delete('/tasks',(req,res)=>{
+    res.send('borrar una lista de tareas');
+})
+
+router.put('/tasks',(req,res)=>{
+    res.send('actualizar una lista de tareas');
+})
+
+
 
 module.exports = router;

c87a2cf37a9bcf4afab40fbcc6c4b8a2fe48a61b
Gary122 <jc_alt64@hotmail.com>
2023-06-02 13:21:29 -0500

    Rutas y temas

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
+import Menu from "./components/Navbar"
+import Container from '@mui/material/Container';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TaskList/>}/>
-        <Route path="/tasks/new" element={<TaskForm/>} />
-      </Routes>
+    <BrowserRouter>   
+      <Menu/>
+      <Container>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/tasks/new" element={<TaskForm />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   )
 }

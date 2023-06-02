466c7a6b6066da3bb038ccfa3a094122418bd3a1
Gary122 <jc_alt64@hotmail.com>
2023-06-02 12:45:08 -0500

    creando rutas front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+
 export default function App() {
   return (
-    <div>
-      hola mundo
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TaskList/>}/>
+        <Route path="/tasks/new" element={<TaskForm/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }

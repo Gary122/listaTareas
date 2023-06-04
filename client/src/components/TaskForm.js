1787718bc0fd65ffcd1a5a804e47f7453cd42e66
Gary122 <jc_alt64@hotmail.com>
2023-06-04 00:58:54 -0500

    diseno formulario

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,7 +1,52 @@
+import { Button, Card, CardContent, Grid, TextField, Typography } from "@mui/material"
+
+
 export default function TaskForm() {
   return (
-    <div>
-      TaskForm
-    </div>
+    <Grid container direction="column" alignItems="center" justifyContent="center">
+      <Grid item xs={3}>
+        <Card sx={{ mt: 5 }}
+          style={{
+            backgroundColor: '#1e272e',
+            padding: '1rem'
+          }}>
+          <Typography variant='5' textAlign='center' color='white'>  Create Task </Typography>
+          <CardContent>
+            <form>
+              <TextField
+                variant='filled'
+                label='write your tittle'
+                sx={{
+                  display: 'block',
+                  margin: '.5rem 0'
+                }}
+                inputProps={{style:{color:"white"}}}
+                InputLabelProps={{style:{color:"white"}}}
+              />
+
+              <TextField
+                variant='filled'
+                label='Write your description'
+                multiline
+                rows={4}
+                sx={{
+                  display: 'block',
+                  margin: '.5rem 0'
+                }}
+                inputProps={{style:{color:"white"}}}
+                InputLabelProps={{style:{color:"white"}}}
+              />
+
+              <Button variant='contained' color='primary' type='submit'>
+                Save
+              </Button>
+
+            </form>
+          </CardContent>
+        </Card>
+
+      </Grid>
+
+    </Grid>
   )
 }
